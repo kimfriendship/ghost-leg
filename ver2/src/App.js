ce77621b37deac1b6804d1f5004fa55db4678f30
kimfriendship <kfriendship94@gmail.com>
2020-12-03 12:49:57 +0900

    :fire: Finally fix git

---
+++ ver2/src/App.js
@@ -0,0 +1,13 @@
+import Home from "Pages/Home";
+import ResetStyle from "style";
+
+function App() {
+  return (
+    <>
+      <Home />
+      <ResetStyle />
+    </>
+  );
+}
+
+export default App;
---
+++ ver2/src/Components/Counter.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Counter = () => {
+  return <div></div>;
+};
+
+export default Counter;
